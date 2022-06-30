1d855b363912841746414262e11c275b12205a00
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2022-07-01 01:36:06 +0530

    42-max-consecutive-ones ✅

---
+++ 42-max-consecutive-ones/array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int num:nums){
+            if(num==1){
+                count++;
+            }else{
+                count=0;
+            }
+            max=Math.max(max,count);
+        }
+        return max;
+    }
+}
+
+
+// T-> O(N)
+// S -> O(1)
