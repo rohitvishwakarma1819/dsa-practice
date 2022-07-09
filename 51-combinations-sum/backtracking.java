2ca885a6de90133fc714741d95ef4bb3eec173d8
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2022-07-09 14:43:40 +0530

    51- combinations-sum(subset-sum) ✅

---
+++ 51-combinations-sum/backtracking.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+         Arrays.sort(candidates);
+        return combSumUtil(candidates,target,0,0,0);
+    }
+    
+    public List<List<Integer>> combSumUtil(int[] candidates, int target,int lastSum,int index, int count){
+        List<List<Integer>> combinations = new ArrayList<>();
+        
+        for(int i=index;i<candidates.length;++i){
+            int currSum = lastSum+candidates[i];
+            if(currSum == target){
+                ArrayList<Integer> l = new ArrayList<>();
+                for(int j=0;j<count+1;++j) l.add(0);
+                l.set(count,candidates[i]);
+                combinations.add(l);
+            }else if(currSum<target){
+                List<List<Integer>> ll = combSumUtil(candidates,target,currSum,i,count+1);
+                if(ll.size()>0){
+                    for(List<Integer> l: ll){
+                        l.set(count,candidates[i]);
+                        combinations.add(l);
+                    }
+                }
+            }else{
+                break;
+            }
+        }
+        return combinations;
+    }
+}
---
+++ 51-combinations-sum/backtracking2.java
@@ -0,0 +1,34 @@
+// Given an integer array 'ARR' of size 'N' and an integer 'K', return all the subsets of 'ARR' which sum to 'K'.
+// Subset of an array 'ARR' is a tuple that can be obtained from 'ARR' by removing some (possibly all) elements of 'ARR'.
+// Note :
+// The order of subsets is not important. 
+
+// The order of elements in a particular subset should be in increasing order of the index.
+import java.util.*;
+public class Solution 
+{
+    public static ArrayList<ArrayList<Integer>> findSubsetsThatSumToK(ArrayList<Integer> arr, int n, int k) 
+	{
+
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> currList = new ArrayList<>();
+         combSumUtil(arr,k,0,0,ans,currList);
+        return ans;
+    }
+    
+    public static void combSumUtil(ArrayList<Integer> candidates, int target,int lastSum,int index,ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> currList){
+        for(int i=index;i<candidates.size();++i){
+           int currSum = lastSum+candidates.get(i);
+            if(currSum==target){
+                currList.add(candidates.get(i));
+                ans.add(new ArrayList<>(currList));
+                currList.remove(currList.size()-1);
+            }
+                
+                currList.add(candidates.get(i));
+                combSumUtil(candidates,target,currSum,i+1,ans,currList);
+                currList.remove(currList.size()-1);
+            
+        }
+    }
+}
